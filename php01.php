e74b2785cebd87cb7604f172483b0202f9880a81
himelhasan <himelhasan497@gmail.com>
2025-08-05 12:58:43 +0600

    basics

---
+++ php01.php
@@ -0,0 +1,29 @@
+<?php
+
+//variable
+$name         = "bangladesh";
+$age          = 123;
+$myName       = "Himel Hasan";
+$myAge        = 23;
+$isMarried    = true;
+$noOfChildren = 1;
+define( "wife", "Anny" );
+$age      = 30;
+$constant = "constant";
+
+echo $name;
+echo "\n";
+echo " Hello $name";
+
+echo "new age is $age \n";
+echo "new name is $myName \n";
+
+echo "new age is $age  and my wife's name is" . wife;
+echo constant( "wife" );
+echo "\n I love {$constant( "wife" )} \n";
+
+$friends = [];
+
+var_dump( $name );
+var_dump( $age );
+var_dump( $friends );
