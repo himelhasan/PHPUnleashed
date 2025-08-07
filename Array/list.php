79ca4617d8cd03efdfe4b3e400afc7dd55e4bd38
himelhasan <himelhasan497@gmail.com>
2025-08-07 20:26:14 +0600

    rand shuffle

--- Array/list.php
+++ Array/list.php
@@ -6,13 +6,28 @@ $green = $color[1];
 $blue  = $color[2];
 
 list( $red, $green, $blue ) = $color;
-
 echo $green;
+echo "\n";
 
 $student             = ['Fname', 'Lname', 'Email', 'MobileNumber'];
 list( $f, $l, $e, $m ) = ['Fname', 'Lname', 'Email', 'MobileNumber'];
 echo $f;
-
 echo "\n";
+
 $random = mt_rand( 0, 55 );
-echo $random;
+echo "\nRANDOM \n";
+print_r( $random );
+echo "\n";
+
+$numberRange = range( 201, 222 );
+print_r( $numberRange );
+
+shuffle( $numberRange );
+echo "\n   shuffle ki hoise?";
+print_r( $numberRange );
+
+$fruits = ['a' => 'apple', 'b'  => 'banana',
+    'f'            => 'orange', 'p' => 'plum', 'd' => 'dates'];
+
+shuffle( $fruits ); // it shuffles but it loose the key
+                  // print_r( $fruits );
