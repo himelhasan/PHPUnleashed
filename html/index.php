6cdbf536642b30efe232bb02d6d1ad45cc52312e
himelhasan <himelhasan497@gmail.com>
2025-08-08 13:13:03 +0600

    html form php connect

--- html/index.php
+++ html/index.php
@@ -7,19 +7,49 @@
   </head>
   <body>
 
-<section class="text-gray-600 body-font relative">
+<section class="text-gray-600 body-font relative ">
   <div class="container px-5 py-24 mx-auto">
     <div class="flex flex-col text-center w-full mb-12">
       <h1 class="sm:text-3xl text-2xl font-medium
       title-font mb-4 text-gray-900">Contact Us</h1>
 
+
+<div class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg
+shadow-sm hover:bg-gray-100 ">
+<p class="font-normal text-gray-700">
+<?php if (
+        ( isset( $_POST['name'] ) && ( ! empty( $_POST['name'] ) ) )
+    ) {
+        echo $_POST['name'];
+
+}?>
+
+
+</p>
+<p class="font-normal text-gray-700">
+
+
+<?php
+
+    if ( ( isset( $_POST["email"] ) ) && ( ! empty( $_POST["email"] ) ) ) {
+        echo $_POST["email4r5t"];
+
+    }
+
+?>
+
+</p>
+</div>
     </div>
-    <div class="lg:w-1/2 md:w-2/3 mx-auto">
+
+    <form action="" method="POST">
+<div class="lg:w-1/2 md:w-2/3 mx-auto">
       <div class="flex flex-wrap -m-2">
         <div class="p-2 w-1/2">
           <div class="relative">
             <label for="name" class="leading-7 text-sm text-gray-600">Name</label>
-            <input type="text" id="name" name="name" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+            <input type="text" id="name" name="name" class="w-full
+            bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
           </div>
         </div>
         <div class="p-2 w-1/2">
@@ -35,11 +65,14 @@
           </div>
         </div>
         <div class="p-2 w-full">
-          <button class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
+          <button class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">SUBMIT</button>
         </div>
 
       </div>
     </div>
+
+
+    </form>
   </div>
 </section>
 
